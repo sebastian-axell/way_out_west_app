f72a145e41f25279a2f5f7009963c6d5bac18949
Sebastian Axell <sebastian.axell98@gmail.com>
2024-04-07 15:37:00 +0100

    server ready

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,31 @@
 // npm install express axios crypto
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
-
+const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Replace this secret with your actual secret key used to sign JWT tokens
-const access_key = 'your-jwt-secret';
-SECRET_KEY = "hey"
+const secretKey="3"
+const apiKey = "test"
 
 // Middleware to verify JWT tokens
 function verifyToken(req, res, next) {
-    if (!req.headers['api-key']) {
+    if (!req.headers['api-hash']) {
         return res.status(401).json({ error: 'API key is missing' });
     }
 
     try {
-        const encryptedApiKey = req.headers['api-key'];
-        const decipher = crypto.createDecipher('aes-256-cbc', SECRET_KEY);
-        let decryptedApiKey = decipher.update(encryptedApiKey, 'hex', 'utf8');
-        decryptedApiKey += decipher.final('utf8');
-        req.decryptedApiKey = decryptedApiKey;
-        next();
+        const encryptedAPIKey = req.headers['api-hash'];
+        const calculatedHash = crypto.createHmac('sha256', secretKey)
+                                    .update(apiKey)
+                                    .digest('hex');
+        if (calculatedHash === encryptedAPIKey) {
+            console.log('Message integrity verified. Sender is trusted.');
+            next();
+        } else {
+            console.log('Message integrity could not be verified. Potential tampering.');
+            return res.status(404).json({ error: 'Not authorised' }); 
+        }
     } catch (error) {
         console.error('Error decrypting API key:', error);
         return res.status(500).json({ error: 'Internal server error' });
