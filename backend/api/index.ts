4a071f1d07f0b6aebb3e27910bcc591452c4ec51
Sebastian Axell <sebastian.axell98@gmail.com>
2024-04-08 14:31:50 +0100

    added cors

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -38,6 +38,8 @@ function decrypt(encryptedText, keyHex, ivHex) {
 
 // Middleware to verify JWT tokens
 function verifyToken(req, res, next) {
+   console.log(req);
+    
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
