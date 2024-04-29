1139e517eec365423f05ff747b93482e9efd6718
Sebastian Axell <sebastian.axell98@gmail.com>
2024-04-29 17:06:58 +0100

    fixed paths

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -15,7 +15,7 @@ const ivHex = process.env.ivHex;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../../svgs/'); // Destination folder for uploaded SVG files
+      cb(null, '../svgs/'); // Destination folder for uploaded SVG files
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname); // Use original file name
@@ -76,8 +76,7 @@ app.post('/upload', upload.array('svgFiles', 10), (req, res) => {
   });
   
 // Serve the uploaded SVG files statically
-app.use('/media', express.static(path.join(__dirname, 'svgs')));
-
+app.use('/media', express.static(path.join(__dirname, '..', 'svgs')));
 
 // Protected route
 app.get('/protected', verifyToken, (req, res) => {
