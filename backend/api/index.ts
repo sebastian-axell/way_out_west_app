eb05005d52c4fe636f647a224f1d024bb81480da
Sebastian Axell <sebastian.axell98@gmail.com>
2024-04-29 16:53:56 +0100

    changed relative path

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -15,7 +15,7 @@ const ivHex = process.env.ivHex;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../svgs/'); // Destination folder for uploaded SVG files
+      cb(null, '../../svgs/'); // Destination folder for uploaded SVG files
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname); // Use original file name
