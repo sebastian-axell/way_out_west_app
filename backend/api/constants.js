5dae7fe0d00f35b400f88faffb4806b4d2c8e01b
Sebastian Axell <sebastian.axell98@gmail.com>
2024-05-09 15:33:06 +0100

    troubleshooting

--- backend/api/constants.js
+++ backend/api/constants.js
@@ -9,10 +9,12 @@ if (process.env.NODE_ENV == "local"){
   keyHex = process.env.ENCRYPT_KEY_HEX; 
   ivHex = process.env.ENCRYPT_IV_HEX;  
 } else {
-  access_key = process.env.SECRET_KEY;
+  access_key = process.env.apiKey;
   keyHex = process.env.keyHex; 
   ivHex = process.env.ivHex;
 }
+
+console.log(access_key, keyHex, ivHex);
 
 module.exports = {
     access_key,
