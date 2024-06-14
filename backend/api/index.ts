37cc84628cf91b1f860215dafd0fc01641bc381f
Sebastian Axell <sebastian.axell98@gmail.com>
2024-06-14 16:46:25 +0100

    another cors fix

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   if (whitelist.includes(origin)) {
       res.header('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
