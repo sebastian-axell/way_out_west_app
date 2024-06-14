deb6507644d22b17ff3e32c4bb0679d5a28c3fbd
Sebastian Axell <sebastian.axell98@gmail.com>
2024-06-14 16:21:16 +0100

    fixed middleware

--- backend/api/middleware/middleware.js
+++ backend/api/middleware/middleware.js
@@ -17,6 +17,10 @@ const corsOptions = {
 
 
 function verifyApiKey(req, res, next){
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', whitelist); // Replace with your client's origin
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   let auth_header = req.headers['authorization'];
   if (!auth_header || !auth_header.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'API key is missing' });
