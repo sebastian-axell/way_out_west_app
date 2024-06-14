76e488862970c2cb8c050d82317e5252e2008b7b
Sebastian Axell <sebastian.axell98@gmail.com>
2024-06-14 16:51:00 +0100

    another cors fix"

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   // if (whitelist.includes(origin)) {
   //     res.header('Access-Control-Allow-Origin', origin);
   // }
-  res.header('Access-Control-Allow-Origin', 'https://weoutwest.info')
+  res.header('Access-Control-Allow-Origin', 'https://weoutwest.info/')
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
